5915aaec2e9e138433bd17c9deda68aa0e53bb9a
bastiwaterloo <sebastian.waterloo@xtheon.com>
2018-11-14 10:48:04 +0100

    Auditlogserver as standalone

--- src/main/java/edu/hm/dako/chat/auditlogServer/AuditlogServer.java
+++ src/main/java/edu/hm/dako/chat/auditlogServer/AuditlogServer.java
@@ -1,32 +1,51 @@
 package edu.hm.dako.chat.auditlogServer;
 
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.net.*;
 
-public class AuditlogServer implements Runnable {
-  DatagramSocket serverSocket = new DatagramSocket(50003);
-  private final int clientPort;
 
-  public AuditlogServer (int clientPort) throws SocketException {
-    this.clientPort = clientPort;
+public class AuditlogServer implements Runnable {
+  private int port;
+  DatagramSocket serverSocket = new DatagramSocket(this.port);
+
+  public AuditlogServer (int port) throws SocketException {
+    this.port = port;
   }
 
-  public void sendDummyMessage (String message) {
-    try{
-      DatagramPacket datagramPacket = new DatagramPacket(message.getBytes(), message.length(), InetAddress.getLocalHost(), this.clientPort);
-      serverSocket.send(datagramPacket);
-    } catch (SocketException e){
+  public static void main(String [] args) throws SocketException {
+    new Thread(new AuditlogServer(50900)).start();
+  }
+
+  public void writeLog(String message) {
+    String fileName = "C:\\Users\\basst\\Desktop\\chat\\chatApplication\\src\\main\\logs\\log1.txt";
+    try {
+      BufferedWriter writer = new BufferedWriter(new FileWriter(fileName, true));
+      writer.write(message + "\n");
+      message = "";
+      writer.close();
+    } catch (IOException e) {
       e.printStackTrace();
-    } catch (UnknownHostException e) {
+    }
+  }
+
+  @Override
+  public void run() {
+    try (DatagramSocket serverSocket = new DatagramSocket(50900)) {
+      byte[] buffer = new byte[65507];
+      while (true) {
+        DatagramPacket datagramPacket = new DatagramPacket(buffer, 0, buffer.length);
+        serverSocket.receive(datagramPacket);
+        String receivedMessages = new String(datagramPacket.getData());
+        datagramPacket.setData(new byte[65507]);
+        writeLog(receivedMessages);
+      }
+    } catch (SocketException e) {
       e.printStackTrace();
     } catch (IOException e) {
       e.printStackTrace();
     }
-
-  }
-
-  @Override
-  public void run(){
-    System.out.println("running");
   }
 }
