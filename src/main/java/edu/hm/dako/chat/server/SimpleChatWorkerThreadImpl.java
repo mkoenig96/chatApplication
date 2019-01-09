c597497f6cdab7a9cb692932a3a9eb5545de0526
bastiwaterloo <sebastian.waterloo@xtheon.com>
2019-01-09 16:12:10 +0100

    fixed minor error

--- src/main/java/edu/hm/dako/chat/server/SimpleChatWorkerThreadImpl.java
+++ src/main/java/edu/hm/dako/chat/server/SimpleChatWorkerThreadImpl.java
@@ -1,10 +1,8 @@
 package edu.hm.dako.chat.server;
 
-import java.io.IOException;
 import java.net.Socket;
 import java.util.Vector;
 
-import edu.hm.dako.chat.AuditlogServer.Selectors;
 import edu.hm.dako.chat.AuditlogServer.TcpConnector;
 import edu.hm.dako.chat.AuditlogServer.UdpConnector;
 import edu.hm.dako.chat.AuditlogServer.AuditlogPDU;
